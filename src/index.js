19d44d59ef128f9fbe69d3b5aa0f8561424e354f
Shalitha <shalithax@gmail.com>
2020-07-19 18:41:04 +0530

    'implement_structure'

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 600
   });
+
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
